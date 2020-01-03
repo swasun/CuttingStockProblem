6d8d2e8c96ec6a9800245c06a51c0220b6be99df
Stefan Agner <stefan@agner.ch>
2020-01-03 22:18:01 +0100

    fix error handling in main
    this prevents crashes when using a non-existing file name

--- src/main.c
+++ src/main.c
@@ -38,7 +38,8 @@
 #include <assert.h>
 #include <time.h>
 
-void compute_problem_from_file(char *file_name) {
+int compute_problem_from_file(char *file_name) {
+    int ret = 0;
     order **orders;
     int order_count, max_width, best_patterns_number;
     double **best_patterns, *pattern_demand_repartition, obj_value;
@@ -51,24 +52,26 @@ void compute_problem_from_file(char *file_name) {
 
     if (!(is_file_exists(file_name))) {
         printf("[ERROR] The specified file '%s' doesn't exists.\n", file_name);
-        goto clean_up;
+        return -1;
     }
 
     if (!(orders = orders_read_from_file(file_name, &order_count, &max_width))) {
         printf("[ERROR] Failed to load orders from the file '%s'.\n", file_name);
-        goto clean_up;
+        return -1;
     }
 
     begin_clock = clock();
 
     if (!(best_patterns = cutting_stock_compute_best_patterns(orders, order_count, max_width, &best_patterns_number))) {
         printf("[ERROR] The computation of the best patterns failed.\n");
-        goto clean_up;
+        ret = -1;
+        goto err_compute_best_patterns;
     }
 
     if (!(pattern_demand_repartition = cutting_stock_compute(best_patterns, best_patterns_number, orders, order_count, &obj_value))) {
         printf("[ERROR] The resolution of the problem failed.\n");
-        goto clean_up;
+        ret = -1;
+        goto err_compute;
     }
 
     end_clock = clock();
@@ -77,23 +80,29 @@ void compute_problem_from_file(char *file_name) {
 
     cutting_stock_print_solution(best_patterns, best_patterns_number, order_count, pattern_demand_repartition, obj_value, orders, order_count, stdout);
 
-clean_up:
-    free((void *)pattern_demand_repartition);
+    free(pattern_demand_repartition);
+
+err_compute:
     columns_matrix_destroy(best_patterns, best_patterns_number);
+err_compute_best_patterns:
     orders_destroy(orders, order_count);
+    return ret;
 }
 
 int main(int argc, char **argv) {
+    int exit_code = 0;
+
     glp_term_out(GLP_OFF);
 
     if (argc != 2) {
         printf("%s <file_name>\n", argv[0]);
-        return 0;
+        return 1;
     }
 
-    compute_problem_from_file(argv[1]);
+    if (compute_problem_from_file(argv[1]) < 0)
+        exit_code = 1;
 
     glp_free_env();
 
-    return 0;
+    return exit_code;
 }
